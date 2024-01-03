f78623e1ebd748d6734f56b64fdf724f893d9ef3
gilberto martini madeira <gilberto.martini@dmcard.com.br>
2024-01-03 15:45:21 -0300

    inclui conexao com redis

--- fastapi/inventory/main.py
+++ fastapi/inventory/main.py
@@ -1,8 +1,34 @@
 from fastapi import FastAPI
+from redis_om import get_redis_connection
+
+# Replace these values with your Redis server configuration
+redis_host = 'localhost'
+redis_port = 6379
+redis_db = 0
+redis_password = 'admin'  # Leave it empty if there is no password
+
+
+# Set up the Redis configuration
+redis_config = {
+    'host': redis_host,
+    'port': redis_port,
+    'db': redis_db,
+    'password': redis_password,
+}
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
+async def root():
+    test_connection()
     return {"Hello": "Gilberto","numeros":[1,2,3,4,5]}
+
+def test_connection():
+    # Get a connection to the Redis database
+    redis_connection = get_redis_connection(host=redis_host, port=redis_port,db=redis_db)
+
+    # Example usage
+    redis_connection.set('my_key', 'my_value')
+    value = redis_connection.get('my_key')
+    print(f'The value of my_key is: {value}')
--- fastapi/inventory/readme.txt
+++ fastapi/inventory/readme.txt
@@ -10,3 +10,5 @@ The command uvicorn main:app refers to:
 main: the file main.py (the Python "module").
 app: the object created inside of main.py with the line app = FastAPI().
 --reload: make the server restart after code changes. Only do this for development.
+
+docker run -p 6379:6379 -p 8001:8001 redis/redis-stack
