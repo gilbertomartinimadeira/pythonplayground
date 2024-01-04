71aeb70a5f2c2274c291b5c417025d9736d43104
gilberto martini madeira <gilberto.martini@dmcard.com.br>
2024-01-04 10:58:12 -0300

    configura middleware

--- fastapi/inventory/main.py
+++ fastapi/inventory/main.py
@@ -1,34 +1,34 @@
 from fastapi import FastAPI
-from redis_om import get_redis_connection
-
+from redis_om import get_redis_connection,HashModel
+from fastapi.middleware.cors  import CORSMiddleware
 # Replace these values with your Redis server configuration
 redis_host = 'localhost'
 redis_port = 6379
 redis_db = 0
 redis_password = 'admin'  # Leave it empty if there is no password
 
-
-# Set up the Redis configuration
-redis_config = {
-    'host': redis_host,
-    'port': redis_port,
-    'db': redis_db,
-    'password': redis_password,
-}
-
-app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    test_connection()
-    return {"Hello": "Gilberto","numeros":[1,2,3,4,5]}
-
-def test_connection():
-    # Get a connection to the Redis database
-    redis_connection = get_redis_connection(host=redis_host, port=redis_port,db=redis_db)
-
     # Example usage
-    redis_connection.set('my_key', 'my_value')
-    value = redis_connection.get('my_key')
-    print(f'The value of my_key is: {value}')
+    # redis_connection.set('minha_chave', 'meu_valor')
+    # value = redis_connection.get('my_key')
+    #print(f'The value of my_key is: {value}')
+
+app = FastAPI()    
+
+app.add_middleware(CORSMiddleware, 
+                   allow_origins=['http://localhost:3000'],
+                   allow_methods=['*'],
+                   allow_headers=['*'])
+
+redis_connection = get_redis_connection(host=redis_host, port=redis_port,db=redis_db)
+    
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis_connection
+
+@app.get("/products")
+def all():
+    return Product.all_pks()
